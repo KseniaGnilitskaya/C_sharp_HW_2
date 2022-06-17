9e099f4829bee237bde306797175d93ae3b27d06
Ksenia Gnilitskaya <ksuha_87@mail.ru>
2022-06-17 23:53:52 +0300

    Task 2 added

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter the first number: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.Write("Enter the second number: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1*number1 == number2){
+    Console.Write($"{number2} is a square of {number1}");
+}
+else{
+    Console.Write($"{number2} is not a square of {number1}");
+}
