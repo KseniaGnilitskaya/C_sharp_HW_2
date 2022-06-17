81a416bbcdf61d2f38503b0cc208b8d6fdd801a8
Ksenia Gnilitskaya <ksuha_87@mail.ru>
2022-06-18 01:04:16 +0300

    Task 3 added

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter the number from 100 to 999: ");
+int number = int.Parse(Console.ReadLine());
+
+string str = number.ToString();
+Console.Write("The second digit is: ");
+Console.Write(str[1]);
