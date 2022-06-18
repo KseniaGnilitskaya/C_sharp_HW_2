f8b2f3f80a25f4d185d8365bd1750d5062b2243e
Ksenia Gnilitskaya <ksuha_87@mail.ru>
2022-06-19 00:12:33 +0300

    Task 4 added

---
+++ Task_4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter the number: ");
+int number = int.Parse(Console.ReadLine());
+
+string str = number.ToString();
+
+if (number < 99){
+    Console.Write("There is no third digit");
+}
+else{
+    Console.Write($"The third digit is {str[2]}");
+}
