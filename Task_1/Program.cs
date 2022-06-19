aa61cb4349696bc73c5c3c32622069239b331e97
Ksenia Gnilitskaya <ksuha_87@mail.ru>
2022-06-19 19:49:14 +0300

    Tas 1 updated

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -5,6 +5,9 @@ int number = int.Parse(Console.ReadLine());
 if(number == 1 | number == 2 | number == 3 | number == 4 | number == 5 ) {
     Console.Write("No, unfortunately. You have to work.");
 }
-else {
+if(number == 6 | number == 7){
     Console.Write("Yes! It's a day-off :)");
+}
+else {
+    Console.Write("Please, enter number from 1 to 7: ");
 }
